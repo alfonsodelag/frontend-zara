a84b5e4780f4944d2b84c00a7da6b8c45af6f747
LAPTOP-LJB9KAGQ\G513 <alfonsodelag1@gmail.com>
2023-02-27 17:29:49 +0100

    minor type fixes in a11yUtils

--- src/utils/a11yUtils.ts
+++ src/utils/a11yUtils.ts
@@ -1,16 +1,34 @@
-export function filterKeyEnter(handler) {
-	return (e) => {
-		if (e.keyCode === 13) {
-			handler(e);
-		}
-	};
+export function filterKeyEnter(handler: (e: KeyboardEvent) => void) {
+  return (e: KeyboardEvent) => {
+    if (e.key === "Enter") {
+      handler(e);
+    }
+  };
 }
 
-export function accessibleOnClick(handler, tabIndex) {
-	return {
-		role: "button",
-		tabIndex: tabIndex || 0,
-		onKeyDown: filterKeyEnter(handler),
-		onClick: handler,
-	};
+export function accessibleOnClick(
+  handler: (e: KeyboardEvent) => void,
+  tabIndex?: number
+) {
+  const keyDownHandler = (e: KeyboardEvent) => {
+    const keyboardEvent = new KeyboardEvent("keydown", {
+      bubbles: true,
+      cancelable: true,
+      shiftKey: e.shiftKey,
+      ctrlKey: e.ctrlKey,
+      altKey: e.altKey,
+      metaKey: e.metaKey,
+      keyCode: 13,
+    });
+    filterKeyEnter((event: KeyboardEvent) => {
+      handler(event);
+    })(keyboardEvent);
+  };
+
+  return {
+    role: "button",
+    tabIndex: tabIndex || 0,
+    onKeyDown: keyDownHandler,
+    onClick: handler,
+  };
 }
